afb227234ea854b37fa5ec67ba0c696b56aa17b9
Richard <ric17101raguine@gmail.com>
2023-10-11 16:49:07 +0800

    Add ScrollPhysics management in Flutter

--- lib/widgets/pokemon_card_background.dart
+++ lib/widgets/pokemon_card_background.dart
@@ -2,20 +2,27 @@ import 'package:flutter/material.dart';
 
 class PokemonCardBackground extends StatelessWidget {
   final int id;
-
-  const PokemonCardBackground({
-    Key? key,
-    required this.id,
-  }) : super(key: key);
-
+  const PokemonCardBackground({Key? key, required this.id}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Text(
-      "$id",
-      style: TextStyle(
-        fontSize: 101,
-        fontWeight: FontWeight.bold,
-        color: Colors.grey[200],
+    return Hero(
+      tag: "$id",
+      child: Material(
+        color: Colors.white,
+        type: MaterialType.transparency,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(21),
+          ),
+        ),
+        child: Text(
+          "$id",
+          style: TextStyle(
+            fontSize: 101,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey[200],
+          ),
+        ),
       ),
     );
   }
